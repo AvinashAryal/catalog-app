bbe26b19e3cb4cf1a901118653a603bc74437e69
Avinash Aryal <avi@pop-os.localdomain>
2023-02-25 00:15:18 +0545

    started using vx

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,12 @@
-import 'package:catalog_app/models/phonedata.dart';
-import 'package:catalog_app/widgets/drawer.dart';
-import 'package:catalog_app/widgets/item_widgets.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'dart:convert';
+import 'package:catalog_app/models/phonedata.dart';
+import 'package:velocity_x/velocity_x.dart';
+import '../widgets/themes.dart';
+//import 'package:catalog_app/widgets/drawer.dart';
+//import 'package:catalog_app/widgets/item_widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -32,11 +35,56 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-
-        ]
+      body: SafeArea(
+        child: Container(
+          padding: Vx.m32,
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Header(),
+            CatalogModel.items.isEmpty || CatalogModel.items == null?
+              Center(child: CircularProgressIndicator(),):CatalogList(),
+          ]),
+        ),
       ),
-    )
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      "Catalog App".text.xl4.bold.color(MyTheme.darkBluishColor).make(),
+      "Trending Products".text.xl2.make(),
+    ]);
+  }
+}
+
+class CatalogList extends StatelessWidget {
+  const CatalogList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: CatalogModel.items.length,
+      itemBuilder:(context,index) {
+        final catalog = CatalogModel.items[index];
+        return CatalogItem(catalog: catalog);
+      }
+    );
+  }
+}
+
+class CatalogItem
+ extends StatelessWidget {
+  final Item catalog;
+  const CatalogItem
+  ({super.key, required this.catalog});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
